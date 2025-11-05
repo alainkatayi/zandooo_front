79200ba2e7c16f51858f2103f6ababc99916f813
alainkatayi <alainkatayi01@gmail.com>
2025-11-05 14:28:17 +0100

    Création du composant login et implémentation de la connexion d'utilisateur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
         path: '',
         title: 'Home',
         loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
-    }
+    },
+    {
+        path: 'login',
+        title: 'Login',
+        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+    },
 ];
