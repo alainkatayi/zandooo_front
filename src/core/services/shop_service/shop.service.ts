eaa5bb2e9b1424aee23e13a4938489a566d004cb
alainkatayi <alainkatayi01@gmail.com>
2025-12-19 16:01:10 +0100

    Creation dans le service shop, de la méthode createShop, qui permet de faire la création d'une boutique

--- src/core/services/shop_service/shop.service.ts
+++ src/core/services/shop_service/shop.service.ts
@@ -4,18 +4,24 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Shop } from '../../models/shop';
 import { ShopPagination } from '../../models/pagination';
+import { UserLocalService } from '../user_local_service/user-local.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
   private apiUrl = environment.url
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient, private userLocalService:UserLocalService) { }
 
   getAllShops(page:number = 1, page_size = 6):Observable<ShopPagination>{
     let params = new HttpParams()
       .set('page',page.toString())
       .set('page_size',page_size.toString())
     return this.http.get<ShopPagination>(`${this.apiUrl}shops/index/`, {params})
+  }
+
+  createShop(data:FormData):Observable<Shop>{
+    const headers = this.userLocalService.getAuthHeaders()
+    return this.http.post<Shop>(this.apiUrl +'shops/store/', data,{headers})
   }
 }
