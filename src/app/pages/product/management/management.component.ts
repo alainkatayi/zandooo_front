f81e83d0491ceede02fc1548ae9e8cd984f93e85
alainkatayi <alainkatayi01@gmail.com>
2026-04-17 09:23:31 +0100

    feat(product):Recuperation et affichage des produits

---
+++ src/app/pages/product/management/management.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule, NgIf } from '@angular/common';
+import { ProductService } from '../../../../core/services/product_service/product.service';
+import { environment } from '../../../../env/env';
+import { Product } from './../../../../core/models/product';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-management',
+  imports: [CommonModule],
+  templateUrl: './management.component.html',
+  styleUrl: './management.component.css'
+})
+export class ManagementComponent {
+  products!:Product[]
+  url = environment.url
+  page_size = 6
+  current_page = 1
+  next_page_url : string | null = null
+  prev_page_url : string | null = null
+  total_count = 0
+  constructor(private productService:ProductService){}
+
+  ngOnInit(){
+    this.getProductByStore()
+  }
+
+
+  getProductByStore(page:number = 1){
+    this.current_page = page
+    this.productService.getProductByShop(page, this.page_size).subscribe({
+      next:(response)=>{
+        this.products = response
+        console.log("produit",this.products)
+        console.log("RESPONSE",response)
+      },
+      error:(error) =>{
+        console.log("ERROR", error)
+      }
+    })
+  }
+}
