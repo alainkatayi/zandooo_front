8fd7fc23a4dd1dffd4e78aa0b446a28ca1e5a6eb
alainkatayi <alainkatayi01@gmail.com>
2026-04-20 08:28:15 +0100

    fix(shop): modification de la pagesize dans la méthode getAllShops pour recuperer plus de shops

--- src/core/services/shop_service/shop.service.ts
+++ src/core/services/shop_service/shop.service.ts
@@ -13,7 +13,7 @@ export class ShopService {
   private apiUrl = environment.url
   constructor(private http : HttpClient, private userLocalService:UserLocalService) { }
 
-  getAllShops(page:number = 1, page_size = 6):Observable<ShopPagination>{
+  getAllShops(page:number = 1, page_size = 10):Observable<ShopPagination>{
     let params = new HttpParams()
       .set('page',page.toString())
       .set('page_size',page_size.toString())
