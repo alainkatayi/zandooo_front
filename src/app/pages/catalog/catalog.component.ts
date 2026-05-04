0c7b3fe4f866f025b979bd7f8dff58e22d352f97
alainkatayi <alainkatayi01@gmail.com>
2026-05-04 11:52:51 +0100

    feat(catalog): ajout d'un sniper avant l'affichage

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -7,10 +7,11 @@ import { ProductCardComponent } from "../../components/client/product-card/produ
 import { ShopCardComponent } from "../../components/client/shop-card/shop-card.component";
 import { NavbarComponent } from "../../components/client/navbar/navbar.component";
 import { FooterComponent } from "../../components/client/footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-catalog',
-  imports: [ProductCardComponent, ShopCardComponent, NavbarComponent, FooterComponent],
+  imports: [ProductCardComponent, ShopCardComponent, NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
@@ -29,6 +30,9 @@ export class CatalogComponent {
   next_page_url_product: string | null = null
   prev_page_url_product: string | null = null
   total_count_product = 0
+
+  product_is_loading:Boolean = false
+  shop_is_loading:Boolean = false
   constructor(private shopService: ShopService, private productService: ProductService) { }
 
   ngOnInit() {
@@ -37,23 +41,28 @@ export class CatalogComponent {
   }
 
   getAllShops(page: number = 1) {
+    this.shop_is_loading = true
     this.current_page = page
     this.shopService.getAllShops(page, this.page_size).subscribe({
       next: (response) => {
+        this.shop_is_loading = false
         console.log("RESPONSE", response)
         this.shops = response.results
         console.log(this.shops)
       },
       error: (error) => {
+        this.shop_is_loading = false
         console.log("ERROR", error)
       }
     })
   }
 
   getAllProducts(page: number = 1) {
+    this.product_is_loading = true
     this.current_page_product = page
     this.productService.getAllProducts(page, this.page_size).subscribe({
       next: (response) => {
+        this.product_is_loading = false
         this.products = response.results
         this.total_count_product = response.count
         this.next_page_url_product = response.next
@@ -63,6 +72,7 @@ export class CatalogComponent {
       },
 
       error: (error) => {
+        this.product_is_loading = false
         console.log(error)
       }
     })
